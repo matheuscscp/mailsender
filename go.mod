19adc76bfa56b9a23b58ddbb6a9df1324f24cf36
Matheus Pimenta <matheuscscp@gmail.com>
2024-11-22 19:16:57 +0000

    chore: remove authz/n

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22
 
 require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.9.0
-	github.com/coreos/go-oidc/v3 v3.11.0
 	github.com/sendgrid/rest v2.6.9+incompatible
 	github.com/sendgrid/sendgrid-go v3.16.0+incompatible
 	github.com/sirupsen/logrus v1.9.3
@@ -12,7 +11,6 @@ require (
 
 require (
 	github.com/cloudevents/sdk-go/v2 v2.15.2 // indirect
-	github.com/go-jose/go-jose/v4 v4.0.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
@@ -21,8 +19,6 @@ require (
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
-	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
-	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- send_email.go
+++ send_email.go
@@ -4,19 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/sirupsen/logrus"
 
 	"github.com/matheuscscp/mailsender/internal/sendgrid"
-)
-
-const (
-	audience = "mailsender"
-	email    = "client@mail-sender-442416.iam.gserviceaccount.com"
 )
 
 func init() {
@@ -29,49 +22,6 @@ func init() {
 		defer cancel()
 
 		var l logrus.FieldLogger = logrus.StandardLogger()
-
-		authz := r.Header.Get("Authorization")
-		if !strings.HasPrefix(authz, "Bearer ") {
-			const msg = "missing or invalid Authorization header"
-			l.Error(msg)
-			http.Error(w, msg, http.StatusUnauthorized)
-			return
-		}
-		token := strings.TrimPrefix(authz, "Bearer ")
-
-		provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
-		if err != nil {
-			const msg = "error creating google oidc provider"
-			l.WithError(err).Error(msg)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-
-		oidcToken, err := provider.VerifierContext(ctx, &oidc.Config{ClientID: audience}).Verify(ctx, token)
-		if err != nil {
-			const msg = "error verifying token as google oidc token"
-			l.WithError(err).Error(msg)
-			http.Error(w, msg, http.StatusUnauthorized)
-			return
-		}
-
-		var claims struct {
-			Email         string `json:"email"`
-			EmailVerified bool   `json:"email_verified"`
-		}
-		if err := oidcToken.Claims(&claims); err != nil {
-			const msg = "error parsing google oidc token claims"
-			l.WithError(err).Error(msg)
-			http.Error(w, msg, http.StatusUnauthorized)
-			return
-		}
-
-		if !claims.EmailVerified || claims.Email != email {
-			const msg = "forbidden"
-			l.Error(msg)
-			http.Error(w, msg, http.StatusForbidden)
-			return
-		}
 
 		var req struct {
 			Subject          string `json:"subject"`
